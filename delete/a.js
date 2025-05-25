0ec18dd8776b3bd9ea1559f5f5871b17f6f746e1
Ahmad Alsaleh <aalsalh474@gmail.com>
2025-05-25 16:57:30 +0300

    v2

---
+++ delete/a.js
@@ -0,0 +1,11 @@
+
+/*
+<i class="fa-solid fa-money-bill-trend-up"></i>
+
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css');
+
+<link rel="stylesheet" href="../css/404.css"></link>
+
+<a href="/" class="logo">MyWebsite</a>
+
+*/
